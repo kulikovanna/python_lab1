35786438fe250406b561df47aeef61aad7f4d320
Anna <annkulikovaa@gmail.com>
2024-02-22 02:43:19 +0300

    task15-17

--- task15-19.py
+++ task15-19.py
@@ -15,9 +15,23 @@ def find_two_smallest(arr):
     sorted_arr = sorted(arr)
     return sorted_arr[:2]
 
+# Дано вещественное число R и массив вещественных чисел.
+# Найти элемент массива, который наиболее близок к данному числу.
+
+def find_closest(arr, R):
+    closest_element = arr[0]
+    smallest_diff = abs(arr[0] - R)
+    for element in arr:
+        current_diff = abs(element - R)
+        if current_diff < smallest_diff:
+            closest_element = element
+            smallest_diff = current_diff
+    return closest_element
+
 print("Выберите задачу:")
 print("1. Найти уникальный элемент в массиве, где все элементы, кроме одного, повторяются.")
 print("2. Дан целочисленный массив. Необходимо найти два наименьших элемента.")
+print("3. Найти элемент массива, наиболее близкий к заданному числу R.")
 
 choice = input("Введите номер задачи: ")
 
@@ -33,3 +47,10 @@ elif choice == '2':
     array = list(map(int, input_array.split()))
     two_smallest_elements = find_two_smallest(array)
     print("Два наименьших элемента в массиве:", ' '.join(map(str, two_smallest_elements)))
+elif choice == '3':
+    R = float(input("Введите число R: "))
+    print("Введите элементы массива через пробел: ", end="")
+    input_array = input()
+    array = list(map(float, input_array.split()))
+    closest_element = find_closest(array, R)
+    print(f"Элемент, наиболее близкий к {R}: {closest_element}")
