78cbdafe0aa74659d360cfaff1ebd34504f4dd68
Anna <annkulikovaa@gmail.com>
2024-02-21 23:36:08 +0300

    task2.2

--- task2-4.py
+++ task2-4.py
@@ -13,3 +13,16 @@ input_string = input("Введите строку: ")
 shuffled_string = shuffle_string(input_string)
 print("Перемешанная строка:", shuffled_string)
 
+# Функция для проверки, образуют ли прописные символы строки палиндром
+def is_uppercase_palindrome(input_string):
+    # Преобразуем входную строку в верхний регистр
+    input_string_upper = input_string.upper()
+    # Проверяем, является ли строка палиндромом
+    return input_string_upper == input_string_upper[::-1]
+
+input_string = input("Введите строку: ")
+result = is_uppercase_palindrome(input_string)
+if result:
+    print("Прописные символы строки образуют палиндром.")
+else:
+    print("Прописные символы строки не образуют палиндром.")
