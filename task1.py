166c17ebf61e34ce279ef65ba48681e584b199b9
Anna <annkulikovaa@gmail.com>
2024-02-21 22:26:26 +0300

    task1.2

--- task1.py
+++ task1.py
@@ -12,3 +12,16 @@ number = int(input("Введите число: "))
 result = count_non_divisible_by_three_divisors(number)
 print("Количество делителей, не делящихся на 3: ", result)
 
+# Функция 2: Найти минимальную нечетную цифру числа
+def find_min_odd_digit(number):
+    min_odd_digit = float('inf')
+    while number > 0:
+        digit = number % 10
+        if digit % 2 != 0 and digit < min_odd_digit:
+            min_odd_digit = digit
+        number //= 10
+    return min_odd_digit if min_odd_digit != float('inf') else None
+
+number = int(input("Введите число: "))
+result = find_min_odd_digit(number)
+print("Минимальная нечетная цифра: ", result)
