0cb61031d2b1e12e58fa8f94498df7ef5198db8a
Anna <annkulikovaa@gmail.com>
2024-02-22 10:04:02 +0300

    task15-18

--- task15-19.py
+++ task15-19.py
@@ -28,10 +28,27 @@ def find_closest(arr, R):
             smallest_diff = current_diff
     return closest_element
 
+# Для введенного списка положительных чисел построить список всех положительных делителей элементов списка без повторений.
+
+def positive_divisors(numbers):
+    all_divisors = []
+    for num in numbers:
+        num_divisors = [] # делители текущего числа
+        for i in range(1, num + 1):
+            if num % i == 0:
+                num_divisors.append(i)
+        # добавляем список делителей num в общий список всех делителей
+        all_divisors.extend(num_divisors)
+
+    # преобразуем список всех делителей во мн-во, чтобы убрать повторяющиеся элементы,
+    # а затем преобразуем его обратно в список и возвр результат
+    return list(set(all_divisors))
+
 print("Выберите задачу:")
 print("1. Найти уникальный элемент в массиве, где все элементы, кроме одного, повторяются.")
 print("2. Дан целочисленный массив. Необходимо найти два наименьших элемента.")
 print("3. Найти элемент массива, наиболее близкий к заданному числу R.")
+print("4. Найти все положительные делители элементов списка.")
 
 choice = input("Введите номер задачи: ")
 
@@ -54,3 +71,9 @@ elif choice == '3':
     array = list(map(float, input_array.split()))
     closest_element = find_closest(array, R)
     print(f"Элемент, наиболее близкий к {R}: {closest_element}")
+elif choice == '4':
+    print("Введите элементы списка через пробел: ", end="")
+    input_numbers = input()
+    numbers = list(map(int, input_numbers.split()))
+    divisors = positive_divisors(numbers)
+    print("Положительные делители элементов списка без повторений:", divisors)
